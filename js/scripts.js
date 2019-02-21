ea6fc20f4f3c68b0cad52079599e9ce79d852fd4
dorismtae <dorismtae@gmail.com>
2019-02-21 15:44:13 +0300

    added jquer and bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,26 @@
-var add = function(number1, number2) {
-return number1 + number2;
+// Business (or back-end) logic:
+
+var add = function(number1,number2) {
+ return number1 + number2;
 };
-alert(add(9,8));
-var subtract= function (number1,number2){
-return number1 - number2;
+var subtract = function(number1,number2) {
+ return number1 - number2;
 };
-alert (subtract(10,5));
-var add =function (number1,number2){
-return number1+number2;
+var multiply = function(number1, number2) {
+ return number1 * number2;
 };
-var number1= prompt("Enter a number:");
-var number2=prompt ("Enter another number:")
-alert(add(number1,number2));
-var subtract= function (number1,number2){
-return number1-number2;
+var divide = function(number1, number2) {
+ return number1 / number2;
 };
-var number1=parseInt(prompt("Enter a number :"));
-var number2=parseInt(prompt("Enter another number"));
-alert (add (nunber1 , number2));
+
+// Everything below this line is user interface (or front-end) logic:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+ var number1 = parseInt($("#add1").val());
+ var number2 = parseInt($("#add2").val());
+ var result = add(number1, number2);
+$("#output").text(result);
+});
+});
